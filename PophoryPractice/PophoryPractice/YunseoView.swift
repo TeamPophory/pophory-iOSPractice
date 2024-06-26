cda190f0fab51006832326f5836e33bcbffd2e52
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-06-26 22:33:33 +0900

    뷰 생성

---
+++ PophoryPractice/PophoryPractice/YunseoView.swift
@@ -0,0 +1,18 @@
+//
+//  YunseoView.swift
+//  PophoryPractice
+//
+//  Created by 강윤서 on 6/26/24.
+//
+
+import SwiftUI
+
+struct YunseoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YunseoView()
+}
