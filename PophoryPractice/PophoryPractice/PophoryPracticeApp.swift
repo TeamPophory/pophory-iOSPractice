017d57105bf08f503a0469b785f9962928e55c77
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-06-26 22:32:26 +0900

    프로젝트 생성

---
+++ PophoryPractice/PophoryPractice/PophoryPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PophoryPracticeApp.swift
+//  PophoryPractice
+//
+//  Created by 강윤서 on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PophoryPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
