23cda91942547279416ad8e2b9c815464c3719f3
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-07-10 18:42:36 +0900

    앨범 수정 뷰 UI 구현

--- PophoryPractice/PophoryPractice/Yunseo/View/SelectAlbumCoverView.swift
+++ PophoryPractice/PophoryPractice/Yunseo/View/SelectAlbumCoverView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct SelectAlbumCoverView: View {
+    @State private var selectedIcon: AlbumIconModel?
+    let rows = [GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            LazyHGrid(rows: rows, spacing: 18) {
+                ForEach(AlbumIconModel.albumIcons, id: \.id) { element in
+                    element.iconImage
+                        .onTapGesture {
+                            selectedIcon = element
+                        }
+                }
+            }
+            .frame(width: .infinity, height: 50)
+            .background(Color.gray)
+            
+            Spacer()
+            
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: rows, spacing: 16, content: {
+                    ForEach(AlbumCoverModel.albumCovers, id: \.id) { element in
+                        element.coverImage
+                            .resizable()
+                            .clipShape(
+                                .rect(topLeadingRadius: 4,
+                                      bottomLeadingRadius: 4,
+                                      bottomTrailingRadius: 26,
+                                      topTrailingRadius: 26,
+                                      style: .circular)
+                            )
+                            .frame(width: 280, height: 380)
+                        
+                    }
+                })
+            }
+            .frame(width: .infinity, height: 380)
+            .background(Color.red)
+            
+            Spacer()
+        }
     }
 }
 
--- PophoryPractice/PophoryPractice/Yunseo/albumModel.swift
+++ PophoryPractice/PophoryPractice/Yunseo/albumModel.swift
@@ -7,7 +7,48 @@
 
 import SwiftUI
 
-struct albumCoverModel {
+struct AlbumCoverModel: Identifiable {
     let id = UUID()
     let coverImage: Image
+}
+
+struct AlbumIconModel: Identifiable {
+    let id = UUID()
+    let iconImage: Image
+}
+
+struct AlbumSelectIconModel: Identifiable {
+    let id = UUID()
+    let iconImage: Image
+}
+
+extension AlbumCoverModel {
+    static var albumCovers: [AlbumCoverModel] = [
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverFamily1)),
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverFamily2)),
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverLove1)),
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverLove2)),
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverFriends1)),
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverFriends2)),
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverMe1)),
+        AlbumCoverModel(coverImage: Image(.icAlbumCoverMe2)),
+    ]
+}
+
+extension AlbumIconModel {
+    static var albumIcons: [AlbumIconModel] = [
+        AlbumIconModel(iconImage: Image(.albumCover1)),
+        AlbumIconModel(iconImage: Image(.albumCover2)),
+        AlbumIconModel(iconImage: Image(.albumCover3)),
+        AlbumIconModel(iconImage: Image(.albumCover4))
+    ]
+}
+
+extension AlbumSelectIconModel {
+    static var albumSelectIcons: [AlbumSelectIconModel] = [
+        AlbumSelectIconModel(iconImage: Image(.albumCoverProfile1)),
+        AlbumSelectIconModel(iconImage: Image(.albumCoverProfile2)),
+        AlbumSelectIconModel(iconImage: Image(.albumCoverProfile3)),
+        AlbumSelectIconModel(iconImage: Image(.albumCoverProfile4))
+    ]
 }
